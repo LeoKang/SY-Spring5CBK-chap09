aa73d074eabbfba0f5333e8b1611b16b817a3a0f
SYU <xeungjin@gmail.com>
2025-04-04 11:30:41 +0900

    ~ 302 page

--- src/main/java/sp5/chap09_jsp/config/MemberConfig.java
+++ src/main/java/sp5/chap09_jsp/config/MemberConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import sp5.chap09_jsp.spring.ChangePasswordService;
 import sp5.chap09_jsp.spring.MemberDao;
+import sp5.chap09_jsp.spring.ChangePasswordService;
 import sp5.chap09_jsp.spring.MemberRegisterService;
 
 @Configuration
